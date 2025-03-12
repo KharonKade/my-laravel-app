6d2f0269f602985dff0d620c465c06ab7de74d23
M305User <M305User@M305.uc-bcf.edu.ph>
2025-03-12 08:49:53 +0800

    register & login addition

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\NoteController;  
+use App\Http\Controllers\NoteController; 
+use App\Http\Controllers\Auth\LoginController; 
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::resource('notes', NoteController::class);  
+
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+
+Route::get('/register', [UserController::class, 'showRegisterForm']);
+Route::post('/register', [UserController::class, 'register'])->name('register');
+
+Route::get('/login', [UserController::class, 'showLoginForm']);
+Route::post('/login', [UserController::class, 'login'])->name('login');
